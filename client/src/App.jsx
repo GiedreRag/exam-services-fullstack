699b0fffadedcf304457b55e0878a56a36e1bfb5
Giedre <giedre.rageliene@gmail.com>
2023-09-19 09:35:08 +0300

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+        Labas
+    </div>
+  );
+}
+
+export default App;
---
+++ server/dbSetup.js
@@ -0,0 +1,23 @@
+import mysql from 'mysql2/promise';
+import { DB_DATABASE, DB_HOST, DB_PASS, DB_USER } from './env.js';
+
+const database_reset = false;
+
+async function dbSetup() {
+    let connection = await mysql.createConnection({
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASS,
+    });
+
+    if (database_reset) {
+        await connection.execute(`DROP DATABASE IF EXISTS \`${DB_DATABASE}\``);
+    }
+
+    await connection.execute(`CREATE DATABASE IF NOT EXISTS \`${DB_DATABASE}\``);
+    connection.query(`USE \`${DB_DATABASE}\``);
+
+    return connection;
+}
+
+export const connection = await dbSetup();
