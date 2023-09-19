b319ae8adeedaea171eea80a9051f469a3ba222f
Giedre <giedre.rageliene@gmail.com>
2023-09-19 10:36:11 +0300

    Register form, register js, terms page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,22 +4,29 @@ import { Home } from './pages/Home';
 import { Page404 } from './pages/Page404';
 import { UserLayout } from './layout/UserLayout';
 import { Account } from './pages/accounts/Account';
+import { ContextWrapper } from './context/GlobalContext';
+import { Register } from './pages/Register';
+import { Terms } from './pages/Terms';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route Component={PublicLayout}>
-          <Route index path='/' element={<Home />}></Route>
-        </Route>
-        <Route Component={UserLayout}>
-          <Route index path='/paskyra' element={<Account />}></Route>
-        </Route>
-        <Route Component={PublicLayout}>
-          <Route path='*' element={<Page404 />}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ContextWrapper>
+      <BrowserRouter>
+        <Routes>
+          <Route Component={PublicLayout}>
+            <Route index path='/' element={<Home />}></Route>
+            <Route index path='/registracija' element={<Register />}></Route>
+            <Route index path='/taisykles' element={<Terms />}></Route>
+          </Route>
+          <Route Component={UserLayout}>
+            <Route index path='/paskyra' element={<Account />}></Route>
+          </Route>
+          <Route Component={PublicLayout}>
+            <Route path='*' element={<Page404 />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ContextWrapper>
   );
 }
 
