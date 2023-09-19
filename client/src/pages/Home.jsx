6981b38db979c747aba6ff4ecac45ac82cc59ce9
Giedre <giedre.rageliene@gmail.com>
2023-09-19 09:50:41 +0300

    Home, 404 page, footer, header

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+export function Home() {
+    return (
+        <div className="container">
+            <div className="row">
+                <h4 className="col-12 display-1 text-center">Pagrindinis puslapis</h4>
+                <p className="col-12 display-6 text-center">Sveiki!</p>
+            </div>
+        </div>
+    )
+}
