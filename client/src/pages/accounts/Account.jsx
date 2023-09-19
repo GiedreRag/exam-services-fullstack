2409f459e911b15ea550c19d49cc1a5fad67d40e
Giedre <giedre.rageliene@gmail.com>
2023-09-19 09:51:09 +0300

    Layouts, user account page

---
+++ client/src/pages/accounts/Account.jsx
@@ -0,0 +1,10 @@
+export function Account() {
+    return (
+        <div className="container">
+            <div className="row">
+                <h4 className="col-12 display-1 text-center">Sveiki!</h4>
+                <p className="col-12 display-6 text-center">Vartotojo paskyra</p>
+            </div>
+        </div>
+    )
+}
