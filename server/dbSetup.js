84a5f17ef20f23d822adfcd1dbb22257e0935b6a
Giedre <giedre.rageliene@gmail.com>
2023-09-19 10:35:31 +0300

    Dp setup with users and roles

--- server/dbSetup.js
+++ server/dbSetup.js
@@ -1,7 +1,8 @@
 import mysql from 'mysql2/promise';
 import { DB_DATABASE, DB_HOST, DB_PASS, DB_USER } from './env.js';
+import { hash } from './lib/hash.js';
 
-const database_reset = false;
+const database_reset = true;
 
 async function dbSetup() {
     let connection = await mysql.createConnection({
@@ -17,7 +18,76 @@ async function dbSetup() {
     await connection.execute(`CREATE DATABASE IF NOT EXISTS \`${DB_DATABASE}\``);
     connection.query(`USE \`${DB_DATABASE}\``);
 
+    if (database_reset) {
+        await rolesTable(connection);
+        await usersTable(connection);
+
+        await generateRoles(connection);
+        await generateUsers(connection);
+    }
+
     return connection;
+}
+
+async function usersTable(db) {
+    try {
+        const sql = `CREATE TABLE users (
+                        id int(10) NOT NULL AUTO_INCREMENT,
+                        fullname varchar(60) NOT NULL,
+                        email varchar(60) NOT NULL,
+                        password_hash varchar(200) NOT NULL,
+                        role_id int(10) NOT NULL DEFAULT 2,
+                        createdAt timestamp NOT NULL DEFAULT current_timestamp(),
+                        PRIMARY KEY (id),
+                        KEY role_id (role_id),
+                        CONSTRAINT users_ibfk_1 FOREIGN KEY (role_id) REFERENCES roles (id)
+                    ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci`;
+        await db.execute(sql);
+    } catch (error) {
+        console.log("Couldn't create users table.");
+        console.log(error);
+        throw error;
+    }
+}
+
+async function rolesTable(db) {
+    try {
+        const sql = `CREATE TABLE roles (
+                        id int(10) NOT NULL AUTO_INCREMENT,
+                        role varchar(20) NOT NULL,
+                        PRIMARY KEY (id)
+                    ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci`;
+        await db.execute(sql);
+    } catch (error) {
+        console.log("Couldn't create roles table.");
+        console.log(error);
+        throw error;
+    }
+}
+
+async function generateRoles(db) {
+    try {
+        const sql = `INSERT INTO roles (role) VALUES ('admin'), ('user');`;
+        await db.execute(sql);
+    } catch (error) {
+        console.log("Couldn't create roles into a role' table.");
+        console.log(error);
+        throw error;
+    }
+}
+
+async function generateUsers(db) {
+    try {
+        const sql = `INSERT INTO users (fullname, email, password_hash, role_id) 
+                    VALUES ('Jonas Jonaitis', 'jonas@jonas.lt', '${hash('jonas@jonas.lt')}', 1),
+                     ('Ona Onaite', 'ona@ona.lt', '${hash('ona@ona.lt')}', 2),
+                     ('Lukas Lukaitis', 'lukas@lukas.lt', '${hash('lukas@lukas.lt')}', 2);`;
+        await db.execute(sql);
+    } catch (error) {
+        console.log("Couldn't create users into a users' table.");
+        console.log(error);
+        throw error;
+    }
 }
 
 export const connection = await dbSetup();
